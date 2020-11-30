466b6a2d1ce62cb985e0b610788279de4dcfbb71
Himanshu <himanshurajverma549@gmail.com>
2020-11-30 18:30:43 +0530

    initial commit

--- homeJS.js
+++ homeJS.js
@@ -4,6 +4,7 @@ savebtn.addEventListener('click', function () {
     /*event listener when user clicks on the save note button
     it will access the notetxt and save it into the localstorage of the browser
     by using JSON. and show it by calling showNotes */
+    let noteTitle = document.getElementById('noteTitle');//notetitle.
     let notetxt = document.getElementById('notetxt');//notetxt.
     let savednotes = localStorage.getItem('notes');//accessing the notes obj from localstorage.
     if (savednotes == null) {
@@ -13,14 +14,25 @@ savebtn.addEventListener('click', function () {
         /*if note note obj is not null is will pasre it into the Json object form*/
         notesObj = JSON.parse(savednotes);
     };
-    //here it will push the notetext in note obj.
-    notesObj.push(notetxt.value);
-    //converting the json object to string and saving it into localstorage.
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    //setting the notetext value to empty.
-    notetxt.value = "";
-    //again running the showNotes function to show all the saved notes.
-    showNotes();
+    if (noteTitle.value == "" || notetxt.value == "") {
+        alert("Please Enter all the fields!");
+    } else {
+        let NotesdataObj = {
+            title: noteTitle.value,
+            notetxt: notetxt.value
+        };
+        //here it will push the NotesdataObj in note obj.
+        notesObj.push(NotesdataObj);
+        //converting the json object to string and saving it into localstorage.
+        localStorage.setItem('notes', JSON.stringify(notesObj));
+        //setting the noteTitle and notetext value to empty.
+        notetxt.value = "";
+        noteTitle.value = "";
+        //again running the showNotes function to show all the saved notes.
+        showNotes();
+    };
+
+
 });
 
 function showNotes() {
@@ -39,15 +51,15 @@ function showNotes() {
         /*iterating the notes object for every element and add the below html data into the empty html variable. */
         html += `
         <div class="card">
-        <p class="heading" id="noteHeading">mynote-${index + 1}</p>
-        <p class="content" id="noteContent">${element}</p>
+        <p class="heading" id="noteHeading">${index + 1}.${element.title}</p>
+        <p class="content" id="noteContent">${element.notetxt}</p>
         <button type="submit" id="${index}" onclick="deleteNote(this.id)" class="btn">Delete note.</button>
         </div>
         `;
     });
     let notesElem = document.getElementById('myNotes');//accessing the notes container.
     if (notesObj != 0) {
-        /*if notes obj is not 0 it will add the above html inside the innser html of the main notes container.*/ 
+        /*if notes obj is not 0 it will add the above html inside the innser html of the main notes container.*/
         notesElem.innerHTML = html;
     } else {
         /*if notes obj is null it will append the below html into the main notes container. */
@@ -72,21 +84,21 @@ function deleteNote(index) {
 
 /*This is used to search the notes based on their title and content. */
 let searchtxt = document.getElementById('searchtxt');//accessing the search text from the search bar.
-searchtxt.addEventListener('input', function(){
+searchtxt.addEventListener('input', function () {
     /*
     This will lisen when user start typing the character in search bar.
      */
     let inputval = searchtxt.value.toLowerCase();//access the search bar text.
     let notesCard = document.getElementsByClassName('card');//accessing the note containers list.
-    Array.from(notesCard).forEach(function(element) {
+    Array.from(notesCard).forEach(function (element) {
         /**
          * this will iterate over all the note containers match the title and content of the note with the searched text of the search bar.
          */
         let cardTitle = element.children[0].innerText.toLowerCase();
         let cardTxt = element.children[1].innerText;
-        if (cardTxt.includes(inputval)||cardTitle.includes(inputval)) {
+        if (cardTxt.includes(inputval) || cardTitle.includes(inputval)) {
             //if it matched it will set the element display to block.
-            element.style.display = "block";    
+            element.style.display = "block";
         } else {
             //if won't matched it will set the element display to none.
             element.style.display = "none";
